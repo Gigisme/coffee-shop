a3faed53c72dc2b3f0b147231a9b17e54af916b3
Mindaugas Kulinas <kulinasmindaugas@gmail.com>
2023-01-31 22:25:02 +0200

    cors handling

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+use actix_web::{get, http, post, web, App, HttpResponse, HttpServer, Responder};
 
 mod controllers;
 mod db;
@@ -10,15 +11,24 @@ async fn hello() -> impl Responder {
 }
 
 #[post("/")]
-async fn submit(insert_item: web::Json<entities::item::Model>) -> String {
+async fn submit(insert_item: web::Json<entities::item::ModelNoId>) -> String {
     controllers::item::insert(insert_item).await;
     "successful insert".to_string()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(submit))
-        .bind(("127.0.0.1", 8000))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allowed_methods(vec!["POST", "GET"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
+        App::new().wrap(cors).service(hello).service(submit)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
